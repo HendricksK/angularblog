b9f98ec2ed0a85957a7e1ab814862b2569030858
Kurvin Hendricks <kurvinhendricks@gmail.com>
2015-09-06 22:22:24 +0200

    reviewing code

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <html>
   <head>
-    <script src="../angularblog/assets/js/angular.min.js"></script>
-    <script src="../angularblog/assets/js/kickstart.min.js" charset="utf-8"></script>
-    <link rel="stylesheet" src="../angular/assets/css/kickstart.min.css"></link>
+    <script src="/angularblog/assets/js/angular.min.js"></script>
+    <script src="/angularblog/assets/js/kickstart.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" src="/angularblog/assets/css/kickstart.min.css"></link>
   </head>
   <body ng-app>
-    <p>This is your first angular expression: {{ 1 + 2 }}</p>
-
-    Enter your name: <input type="text" ng-model="name"></input>
-    <p>Hello {{name}}!</p>
+    <iframe src="/angularblog/web/views/1.html" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0" >
+    </iframe>
   </body>
 </html>
