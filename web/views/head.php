a925e609f2df0c1f79c95ea83e05413459f1c522
Kurvin Hendricks <kurvinhendricks@gmail.com>
2015-09-07 22:43:23 +0200

    house cleaning added css for mobile

--- web/views/head.php
+++ web/views/head.php
@@ -1,6 +1,7 @@
 <head>
     <script src="/angularblog/assets/js/head.min.js"></script>
     <script src="/angularblog/assets/js/load.js"></script>
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="description" content="www.dev-ninja.com" />
+	<meta name="keywords" content="css, web-development" />
 </head>
